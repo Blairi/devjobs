6d6387cc061b3bb06a85bc4951a80611137daa3b
Blairi <axel.avi.fer@gmail.com>
2022-08-03 14:00:11 -0500

    first commit

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,8 @@
+
+@php
+    $clases = "text-xs text-gray-500 hover:text-gray-900";
+@endphp
+
+<a {{ $attributes->merge(['class' => $clases]) }} >
+    {{ $slot }}
+</a>
---
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -0,0 +1,78 @@
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="space-y-3">
+                    <a href="{{ route('vacantes.show', $vacante->id) }}" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold">
+                        {{ $vacante->empresa }}
+                    </p>
+                    <p class="text-sm text-gray-500">
+                        Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}
+                    </p>
+                </div>
+    
+                <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+
+                    <a 
+                        href="{{ route('candidatos.index', $vacante) }}"
+                        class="text-center bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase"
+                    >
+                    {{ $vacante->candidatos->count() }}
+                    Candidatos</a>
+
+                    <a 
+                        href="{{ route('vacantes.edit', $vacante->id) }}"
+                        class="text-center bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase"
+                    >Editar</a>
+
+                    <button 
+                        wire:click="$emit('mostrarAlerta', {{ $vacante->id }})"
+                        class="text-center bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase"
+                    >Eliminar</button>
+                </div>
+    
+            </div>
+        @empty
+            <p class="p-3 text-center text-sm to-gray-600">No hay vacantes que mostrar.</p>
+        @endforelse
+    
+    </div>
+    
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
+
+</div>
+
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Livewire.on('mostrarAlerta', ( vacanteId ) => {
+            Swal.fire({
+                title: '¿Eliminar vacante?',
+                text: "Una vacante eliminada no se puede recuperar",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, ¡Eliminar!',
+                cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    // Eliminando desde el servidor
+                    Livewire.emit('eliminarVacante', vacanteId);
+
+                    Swal.fire(
+                        'Se eliminó la vacante',
+                        'Eliminado correctamente',
+                        'success'
+                    )
+                }
+            })
+        });
+    </script>
+@endpush
